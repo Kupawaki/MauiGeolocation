cff8fd76deb96934c8ecb0ec704a9d96ab3b874d
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-10-24 14:28:29 -0500

    Initial Commit

---
+++ MauiGeolocation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGeolocation;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiGeolocation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiGeolocation;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
