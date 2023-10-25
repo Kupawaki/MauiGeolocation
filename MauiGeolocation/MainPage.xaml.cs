7231a6d78c96ae50456d7c51353f192d74bae34b
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-10-25 11:12:04 -0500

    Permissions are instantly denied for no reason. Need to fix

--- MauiGeolocation/MainPage.xaml.cs
+++ MauiGeolocation/MainPage.xaml.cs
@@ -15,6 +15,7 @@ public partial class MainPage : ContentPage
 
 	private async void GetCurrentLocation(Object sender, EventArgs e)
 	{
+		await CheckAndRequestLocationPermission();
 		await GetCurrentLocationTask();
 	}
 
@@ -54,6 +55,30 @@ public partial class MainPage : ContentPage
 
 		}
 	}
+
+    public async Task<PermissionStatus> CheckAndRequestLocationPermission()
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        if (status == PermissionStatus.Granted)
+            return status;
+
+        if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            // Prompt the user to turn on in settings
+            // On iOS once a permission has been denied it may not be requested again from the application
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.LocationWhenInUse>())
+        {
+            // Prompt the user with additional information as to why the permission is needed
+        }
+
+        status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        return status;
+    }
 }
 
 
